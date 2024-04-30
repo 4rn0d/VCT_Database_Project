8cfe50c391a1cecdb7a0cff710d5eb8e8d0802fb
4rn0d <arnaudt09@gmail.com>
2024-04-30 09:58:22 -0400

    Initial commit

---
+++ ProjetFinal_2236734/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjetFinal_2236734.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
