91c925a4d3a6b528a11c9d8ba7194c85ea135428
Arnaud Turcotte <arnaudt09@gmail.com>
2024-05-21 19:10:06 -0400

    FCT_Migration_V1_6

---
+++ ProjetFinal_2236734/SQL_Scripts/V1_6__Performance.sql
@@ -0,0 +1,7 @@
+﻿CREATE NONCLUSTERED INDEX IX_Joueur_JoueurID
+ON Equipes.Joueur(JoueurID DESC)
+-- J'ai décider de mettre un index sur joueurID, car c'est, de loin, la table ayant le plus d'entré, alors les avoir triées dès le départ aide la performance.
+
+CREATE NONCLUSTERED INDEX IX_Tournois_DateDebut
+ON Competition.Tournois(DateDebut DESC)
+-- J'ai décider de mettre un index sur DateDebut. Puisqu'Il y a beaucoup de date dans cette table, prioriser l'index sur une seul de ses date aide la performance.
